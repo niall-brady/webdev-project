2db347ffc6911592c9e7efdec070b82bb0ff95b3
Katie J <91962970+katjame@users.noreply.github.com>
2021-12-22 17:06:39 +0000

    made error message more descriptive with timestamp

--- src/components/shared/showError.js
+++ src/components/shared/showError.js
@@ -1,6 +1,14 @@
 //showError
 
 //use same format as showLoading
-const showError = <h2 className="Loading">Error! No data to show</h2>
+const showError = (
+    <div align="center"> 
+        <h2 className="Loading">
+            Network Error! No data to show as unable to contact qREST
+            <br/>
+            Disconnected At: {Date().toLocaleString()}
+         </h2> 
+    </div> 
+)
 
 export {showError}
